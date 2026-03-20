c4184fda5a964c7d76d49dccbfb8ae64d643a1fc
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-20 19:38:04 +0530

    67662

--- manage.py
+++ manage.py
@@ -1,7 +1,26 @@
-git add orders/models,py orders/__init__.py 
+from django.db import models
 
-git commit -m "feat: add orderStatus model and predefined status constants
+class OrderStatus(models.Model):
+    name = models.charField(max_length=50, unique=True)
 
-- Created OrderStatus model with unique name field
-- Added status constants (Pending, Processing, Completed, Cancelled)
-- Prepared structure for order status management"
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+
+    table_number = models.IntegerField(null=True, blank=True)
+    customer_name = models.CharField(max_length=100, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.ForeignKey(
+        orderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='orders'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.status}"
