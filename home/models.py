61c20fe74fb849e30313ddeb2602538672438014
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-24 17:52:57 +0530

    68233

--- home/models.py
+++ home/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    # ... existing fields like created_at, customer, etc. ...
+
+    def get_unique_item_names(self) -> list:
+        """
+        Retrieves a list of unique names for all MenuItems in this order.
+        """
+        # 1. Access related OrderItems (using the default _set manager)
+        # 2. Extract the name from the linked MenuItem
+        # 3. Use a set to filter out duplicates automatically
+        item_names = {
+            item.menu_item.name 
+            for item in self.orderitem_set.all()
+        }
+        
+        # Return as a sorted list for consistent output
+        return sorted(list(item_names))
