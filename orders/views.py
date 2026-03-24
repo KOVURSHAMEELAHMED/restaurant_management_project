584b607fc33b7ac0a57411a99992fc5d94bdf28f
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-24 17:05:18 +0530

    68145

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+class MenuItemPagination(PageNumberPagination):
+    page_size = 10  # Adjust based on your needs
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    pagination_class = MenuItemPagination
+    
+    # Configure the SearchFilter
+    filter_backends = [filters.SearchFilter]
+    # This uses __icontains under the hood for case-insensitive partial matches
+    search_fields = ['name']
