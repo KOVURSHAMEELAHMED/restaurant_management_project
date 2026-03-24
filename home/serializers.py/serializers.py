5a2f212f4672882b0fea3216652b6f086e395e9b
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-24 17:21:54 +0530

    68223

---
+++ home/serializers.py/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import MenuItem
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'name', 'description', 'price', 'is_available']
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price must be a positive number.")
+        return value
