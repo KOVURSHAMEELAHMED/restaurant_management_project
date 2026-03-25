d5ce6188e0b420e6dcbb7ed23a6129bb6cf51219
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-25 18:10:47 +0530

    68231

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializer
 
-# Create your views here.
+class AvailableTablesAPIView(generics.ListAPIView):
+    # Filter the queryset so only available tables are returned
+    queryset = Table.objects.filter(is_available=True)
+    serializer_class = TableSerializer
