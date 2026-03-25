8c44a3fcff4a05fe846dc0751ae59f284dd50a60
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-25 18:11:16 +0530

    68231

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import AvailableTablesAPIView
 
 urlpatterns = [
-    
+    # Map the URL to our new view
+    path('api/tables/available/', AvailableTablesAPIView.as_view(), name='available_tables_api'),
 ]
