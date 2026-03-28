fc1456b3758fed92516d61b031694574f053030b
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-28 18:22:47 +0530

    68433

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class ContactFormSubmission(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # Add this line:
+    is_daily_special = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
