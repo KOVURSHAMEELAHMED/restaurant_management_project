e8c3c61b1a6b0b37e4544036a1c60cec1740c736
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-28 18:27:36 +0530

    68434

--- home/models.py
+++ home/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    # Add this line:
-    is_daily_special = models.BooleanField(default=False)
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
