3adfc9acf49298362aefa039acae427a6b3a07d7
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-28 18:30:30 +0530

    68434

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import *
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    
+    path('admin/', admin.site.urls),
+    path('api/', include('home.urls')), # Access via /api/categories/
 ]
