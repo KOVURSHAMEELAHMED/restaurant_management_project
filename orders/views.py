86c49c4737aadcfb0c4233776e16b9e6c5e09912
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-28 18:34:55 +0530

    68436

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,22 @@
-from rest_framework.generics import ListAPIView
-from .models import PaymentMethod
-from .serializers import PaymentMethodSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Order
+from .serializers import OrderStatusSerializer
 
-class PaymentMethodListView(ListAPIView):
-    """
-    Returns a list of all active payment methods.
-    """
-    serializer_class = PaymentMethodSerializer
+class OrderStatusUpdateView(generics.UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderStatusSerializer
     
-    def get_queryset(self):
-        return PaymentMethod.objects.filter(is_active=True)
+    # We use 'patch' because we are only updating one field
+    def patch(self, request, *args, **kwargs):
+        order_id = kwargs.get('pk')
+        try:
+            order = Order.objects.get(pk=order_id)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
