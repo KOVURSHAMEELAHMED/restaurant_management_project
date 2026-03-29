83b4c582fd37dc906a377e210ba6bda22b29d11a
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-29 17:56:41 +0530

    68606

--- orders/views.py
+++ orders/views.py
@@ -1,22 +1,26 @@
-from rest_framework import generics, status
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Order
-from .serializers import OrderStatusSerializer
 
-class OrderStatusUpdateView(generics.UpdateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderStatusSerializer
-    
-    # We use 'patch' because we are only updating one field
-    def patch(self, request, *args, **kwargs):
-        order_id = kwargs.get('pk')
-        try:
-            order = Order.objects.get(pk=order_id)
-        except Order.DoesNotExist:
-            return Response({"error": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.get_serializer(order, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET'])
+def get_order_status(request, order_id):
+    """
+    Retrieve the current status of an order by ID.
+    """
+    try:
+        # Retrieve the order or return 404 if not found
+        order = Order.objects.get(pk=order_id)
+        
+        # Return a simple JSON object
+        return Response({
+            "order_id": order.id,
+            "status": order.status
+        }, status=status.HTTP_200_OK)
+        
+    except Order.DoesNotExist:
+        return Response(
+            {"error": f"Order with ID {order_id} not found."}, 
+            status=status.HTTP_404_NOT_FOUND
+        )
