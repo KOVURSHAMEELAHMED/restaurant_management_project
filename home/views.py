cbc1c99c4dba6abca0d453d7c7a071089d305545
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-29 18:05:34 +0530

    68603

--- home/views.py
+++ home/views.py
@@ -1,11 +1,21 @@
-from rest_framework import viewsets
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework import generics
+from .models import UserReview
+from .serializers import UserReviewSerializer
 
-class MenuCategoryViewSet(viewsets.ModelViewSet):
-    """
-    A viewset that provides default `create()`, `retrieve()`, `update()`,
-    `partial_update()`, `destroy()` and `list()` actions.
-    """
-    queryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class UserReviewListCreateView(generics.ListCreateAPIView):
+    serializer_class = UserReviewSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned reviews to a given menu item,
+        by filtering against a `menu_item` query parameter in the URL.
+        """
+        queryset = UserReview.objects.all()
+        menu_item_id = self.request.query_params.get('menu_item')
+        if menu_item_id is not None:
+            queryset = queryset.filter(menu_item_id=menu_item_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        # Automatically set the user to the currently logged-in user
+        serializer.save(user=self.request.user)
