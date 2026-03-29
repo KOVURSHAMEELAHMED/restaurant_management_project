d65a498655e0db8a3e7ca934db96fc5abbdc2e5d
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-29 18:01:22 +0530

    68605

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuCategoryViewSet
 
+# Routers automatically map the standard CRUD actions to URL patterns
 router = DefaultRouter()
-router.register(r'categories', MenuCategoryViewSet)
+router.register(r'categories', MenuCategoryViewSet, basename='menucategory')
 
 urlpatterns = [
     path('', include(router.urls)),
