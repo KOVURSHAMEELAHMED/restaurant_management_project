9771cff25e778dd17833f2e3bf80528b002c53b4
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-29 17:57:02 +0530

    68606

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderStatusUpdateView
+from .views import get_order_status
 
 urlpatterns = [
-    path('orders/<int:pk>/status/', OrderStatusUpdateView.as_view(), name='update-order-status'),
+    # Path parameter <int:order_id> matches the argument in the view
+    path('orders/<int:order_id>/status/', get_order_status, name='order-status'),
 ]
