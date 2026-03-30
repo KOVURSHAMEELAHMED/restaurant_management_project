70dc8b3c91d7a264956f359b79533d94bde45e41
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-30 18:37:54 +0530

    68803

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-class MenuItem(models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    is_available = models.BooleanField(default=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    opening_hours = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
