dcbc01b1e2c4371591385df543a0035328dbbd7e
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-03-31 19:26:46 +0530

    68931

--- products/models.py
+++ products/models.py
@@ -1,8 +1,14 @@
-
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class MenuCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username} - {self.rating} Stars"
