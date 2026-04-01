464ca5e42e22340e069918d03c0f8385064e0ef0
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-01 11:31:44 +0530

    69007

--- orders/views.py
+++ orders/views.py
@@ -1,14 +1,8 @@
 from rest_framework import generics
-from rest_framework.pagination import PageNumberPagination
-from .models import Review
-from .serializers import ReviewSerializer
+from .models import Order
+from .serializers import OrderStatusSerializer
 
-class ReviewPagination(PageNumberPagination):
-    page_size = 10  # Number of reviews per page
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-class ReviewListView(generics.ListAPIView):
-    queryset = Review.objects.all().order_by('-created_at')
-    serializer_class = ReviewSerializer
-    pagination_class = ReviewPagination
+class OrderStatusView(generics.RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderStatusSerializer
+    lookup_field = 'short_id'  # This tells DRF to use short_id in the URL
