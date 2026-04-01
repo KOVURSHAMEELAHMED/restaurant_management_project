ee0311dacd6a371dbef7390e2859d8ca36407ad6
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-01 11:25:36 +0530

    69008

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,14 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from .models import Review
 from .serializers import ReviewSerializer
 
-class ReviewCreateView(generics.CreateAPIView):
-    queryset = Review.objects.all()
+class ReviewPagination(PageNumberPagination):
+    page_size = 10  # Number of reviews per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+class ReviewListView(generics.ListAPIView):
+    queryset = Review.objects.all().order_by('-created_at')
     serializer_class = ReviewSerializer
+    pagination_class = ReviewPagination
