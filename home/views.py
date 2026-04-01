c3bbb9cff268044f0d011a60c650b8f1efee9076
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-01 11:38:08 +0530

    69006

--- home/views.py
+++ home/views.py
@@ -1,21 +1,22 @@
 from rest_framework import generics
-from .models import UserReview
-from .serializers import UserReviewSerializer
+from rest_framework.pagination import PageNumberPagination
+from .models import Review
+from .serializers import ReviewSerializer
 
-class UserReviewListCreateView(generics.ListCreateAPIView):
-    serializer_class = UserReviewSerializer
+class ReviewPagination(PageNumberPagination):
+    page_size = 10  # Number of reviews per page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
-    def get_queryset(self):
-        """
-        Optionally restricts the returned reviews to a given menu item,
-        by filtering against a `menu_item` query parameter in the URL.
-        """
-        queryset = UserReview.objects.all()
-        menu_item_id = self.request.query_params.get('menu_item')
-        if menu_item_id is not None:
-            queryset = queryset.filter(menu_item_id=menu_item_id)
-        return queryset
+class ReviewListView(generics.ListAPIView):
+    queryset = Review.objects.all().order_by('-created_at')
+    serializer_class = ReviewSerializer
+    pagination_class = ReviewPagination
 
-    def perform_create(self, serializer):
-        # Automatically set the user to the currently logged-in user
-        serializer.save(user=self.request.user)
+from rest_framework import generics
+from .models import OpeningHour
+from .serializers import OpeningHourSerializer
+
+class OpeningHourListView(generics.ListAPIView):
+    queryset = OpeningHour.objects.all()
+    serializer_class = OpeningHourSerializer
