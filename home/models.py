7d266818fa1ebaf46b5134d4a3361c4bb3ce79bc
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-03 10:24:21 +0530

    69026

--- home/models.py
+++ home/models.py
@@ -1,18 +1,11 @@
 from django.db import models
 
-class OpeningHour(models.Model):
-    WEEKDAYS = [
-        (0, 'Monday'), (1, 'Tuesday'), (2, 'Wednesday'),
-        (3, 'Thursday'), (4, 'Friday'), (5, 'Saturday'), (6, 'Sunday'),
-    ]
-    
-    day = models.IntegerField(choices=WEEKDAYS, unique=True)
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
-    is_closed = models.BooleanField(default=False)  # For holidays or specific closed days
+class Restaurant(models.Model):
+    name = models.CharField(max_length=255)
+    # ... other fields ...
 
-    class Meta:
-        ordering = ['day']
-
-    def __str__(self):
-        return f"{self.get_day_display()}: {self.opening_time} - {self.closing_time}"
+    def get_total_menu_items(self):
+        """
+        Returns the total number of menu items associated with this restaurant.
+        """
+        return self.menu_items.count()
