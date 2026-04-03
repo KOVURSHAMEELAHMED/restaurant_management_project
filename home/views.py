371954e3f5a0072a11b3c55bccbfb3c5e823f24a
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-03 10:26:09 +0530

    69025

--- home/views.py
+++ home/views.py
@@ -1,22 +1,7 @@
 from rest_framework import generics
-from rest_framework.pagination import PageNumberPagination
-from .models import Review
-from .serializers import ReviewSerializer
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-class ReviewPagination(PageNumberPagination):
-    page_size = 10  # Number of reviews per page
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-class ReviewListView(generics.ListAPIView):
-    queryset = Review.objects.all().order_by('-created_at')
-    serializer_class = ReviewSerializer
-    pagination_class = ReviewPagination
-
-from rest_framework import generics
-from .models import OpeningHour
-from .serializers import OpeningHourSerializer
-
-class OpeningHourListView(generics.ListAPIView):
-    queryset = OpeningHour.objects.all()
-    serializer_class = OpeningHourSerializer
+class MenuItemListView(generics.ListAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
