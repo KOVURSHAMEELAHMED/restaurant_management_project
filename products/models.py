fd8b10db428b169e4ed98afa486ed58d1a117998
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-03 10:50:24 +0530

    69167

--- products/models.py
+++ products/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
-    rating = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(5)]
-    )
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=50) # Or models.ForeignKey(Cuisine, ...)
+    # ... other fields ...
 
-    def __str__(self):
-        return f"{self.user.username} - {self.rating} Stars"
+    @classmethod
+    def get_items_by_cuisine(cls, cuisine_type):
+        """
+        Returns a queryset of menu items matching the specified cuisine type.
+        """
+        return cls.objects.filter(cuisine__iexact=cuisine_type)
