2bf813f1474c6b8234f26064504f421e886b54d7
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-05 19:01:42 +0530

    69227

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,16 +1,7 @@
 from rest_framework import serializers
-from .models import Order, OrderItem
+from .models import MenuItem
 
-class OrderItemSerializer(serializers.ModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
-        model = OrderItem
-        fields = ['product_name', 'quantity', 'price']
-
-class OrderHistorySerializer(serializers.ModelSerializer):
-    # Using the method we created earlier for the count
-    item_count = serializers.IntegerField(source='get_total_item_count', read_only=True)
-    items = OrderItemSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Order
-        fields = ['id', 'date_ordered', 'status', 'total_amount', 'item_count', 'items']
+        model = MenuItem
+        fields = ['id', 'name', 'description', 'price', 'is_available']
