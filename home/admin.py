bea0262793cd85eebc34074c32b7fb28a5ffc8b0
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-06 13:21:35 +0530

    69302

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Coupon
 
-# Register your models here.
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    # Displays the toggle in the list view
+    list_display = ('code', 'discount', 'is_active') 
+    
+    # Allows you to toggle it without clicking into the coupon details
+    list_editable = ('is_active',) 
+    
+    # Adds a sidebar filter for quick management
+    list_filter = ('is_active',)
