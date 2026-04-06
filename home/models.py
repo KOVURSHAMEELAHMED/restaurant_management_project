7dcd51a4036caea7405e2e28ac797e48c373aa8c
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-06 14:01:43 +0530

    69338

--- home/models.py
+++ home/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
-class FAQ(models.Model):
-    question = models.CharField(max_length=255)
-    answer = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.question
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    is_available = models.BooleanField(default=True)
+    # ... other fields ...
