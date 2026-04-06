3a6dee9beaad84df982e5f914d540a312b3e1d0d
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-06 13:50:32 +0530

    69340

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,14 +1,10 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Review
 
-class OrderStatusUpdateSerializer(serializers.ModelSerializer):
+class ReviewSerializer(serializers.ModelSerializer):
+    # This pulls the 'username' from the related User model
+    user_name = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
-        model = Order
-        fields = ['status']
-
-    def validate_status(self, value):
-        # Optional: Add custom logic (e.g., can't move from 'Completed' to 'Pending')
-        valid_statuses = [choice[0] for choice in Order.STATUS_CHOICES]
-        if value not in valid_statuses:
-            raise serializers.ValidationError("Invalid status selection.")
-        return value
+        model = Review
+        fields = ['id', 'user_name', 'rating', 'comment', 'created_at']
