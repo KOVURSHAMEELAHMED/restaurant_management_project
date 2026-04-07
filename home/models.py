86a0e8d5510ef486121b22afab2025fb950f88d7
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-07 20:39:44 +0530

    69408

--- home/models.py
+++ home/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    is_available = models.BooleanField(default=True)
-    # ... other fields ...
+    # ... existing fields (name, price, etc.) ...
+    calories = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
