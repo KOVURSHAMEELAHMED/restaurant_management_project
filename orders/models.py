bfeb128339bb7af278038bf59e3a7ab468c2e9aa
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-07 20:08:14 +0530

    69412

--- orders/models.py
+++ orders/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Review(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField() # e.g., 1-5
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.user.username} - {self.rating} Stars"
+        return f"Table {self.table_number} (Capacity: {self.capacity})"
