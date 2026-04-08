af901fe457ad6285d05c4405c8aae783f378a2ae
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-08 18:46:25 +0530

    69413

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
-class MenuItem(models.Model):
-    # ... existing fields (name, price, etc.) ...
-    calories = models.IntegerField(null=True, blank=True)
+from django.db import models
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
