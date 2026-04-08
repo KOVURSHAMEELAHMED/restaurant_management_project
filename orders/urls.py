84eecae239ec072d623c922bb91b4a566ae2a8d7
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-08 18:49:39 +0530

    69417

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuItemViewSet
+from .views import MenuCategoryViewSet
 
 router = DefaultRouter()
-router.register(r'menu-items', MenuItemViewSet, basename='menuitem')
+router.register(r'categories', MenuCategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
