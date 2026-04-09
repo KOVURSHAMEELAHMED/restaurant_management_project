f81350d026f6b29ff04c79a36f7414b89db2c4d1
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-09 19:45:47 +0530

    69504

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
-from .models import MenuItem
+from .models import Order
 
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category', 'is_available', 'allergens')
-    list_filter = ('category', 'is_available')
-    search_fields = ('name', 'description', 'allergens')
-    list_editable = ('price', 'is_available')
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'customer_name', 'order_date', 'status', 'total_amount', 'has_notes')
+    list_filter = ('status', 'order_date')
+    search_fields = ('order_number', 'customer_name', 'customer_email', 'customer_phone', 'customer_notes')
+    list_editable = ('status',)
+    readonly_fields = ('order_number', 'created_at', 'updated_at')
     
-    # If using ManyToManyField with Allergen model:
-    # filter_horizontal = ('allergens',)
+    fieldsets = (
+        ('Order Information', {
+            'fields': ('order_number', 'customer_name', 'customer_email', 'customer_phone')
+        }),
+        ('Order Details', {
+            'fields': ('total_amount', 'status')
+        }),
+        ('Customer Notes', {
+            'fields': ('customer_notes',),
+            'classes': ('wide',),
+            'description': 'Any special instructions, delivery notes, or allergy information from the customer'
+        }),
+        ('Timestamps', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_notes(self, obj):
+        return bool(obj.customer_notes)
+    has_notes.boolean = True
+    has_notes.short_description = 'Has Notes'
 
-admin.site.register(MenuItem, MenuItemAdmin)
+admin.site.register(Order, OrderAdmin)
