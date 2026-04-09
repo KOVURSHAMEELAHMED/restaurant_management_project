50f3d005cd35458f420baad4afe4e53658407fae
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-09 19:14:28 +0530

    69451

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Table  # Importing your Table model
+from .models import Table
 
-@admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
-    list_display = ('table_number', 'capacity', 'is_available') # Optional: helps you see details at a glance
+    list_display = ('table_number', 'capacity', 'is_available', 'location')
+    list_filter = ('is_available', 'location')
+    search_fields = ('table_number', 'location')
+    list_editable = ('is_available', 'capacity')
+    list_per_page = 20
+    ordering = ('table_number',)
+    
+    fieldsets = (
+        ('Table Information', {
+            'fields': ('table_number', 'capacity', 'location')
+        }),
+        ('Status', {
+            'fields': ('is_available',)
+        }),
+    )
+
+# Register the model with the custom admin class
+admin.site.register(Table, TableAdmin)
