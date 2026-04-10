a1920f1dee8855a9a9a20042c42711b840bb2819
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-10 15:10:53 +0530

    69503

--- home/admin.py
+++ home/admin.py
@@ -1,22 +1,5 @@
 from django.contrib import admin
-from .models import Table
+from .models import Restaurant
 
-class TableAdmin(admin.ModelAdmin):
-    list_display = ('table_number', 'capacity', 'is_available', 'location')
-    list_filter = ('is_available', 'location')
-    search_fields = ('table_number', 'location')
-    list_editable = ('is_available', 'capacity')
-    list_per_page = 20
-    ordering = ('table_number',)
-    
-    fieldsets = (
-        ('Table Information', {
-            'fields': ('table_number', 'capacity', 'location')
-        }),
-        ('Status', {
-            'fields': ('is_available',)
-        }),
-    )
-
-# Register the model with the custom admin class
-admin.site.register(Table, TableAdmin)
+# Register your models here.
+admin.site.register(Restaurant)
