673f19f950479ea8f27f8ac33f02e5ecfeab6bd3
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-10 15:40:07 +0530

    69596

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,12 @@
-from rest_framework import serializers
-from .models import Item
 
-class ItemSerializer(serializers.ModelSerializer):
+from rest_framework import serializers
+from .models import Cuisine
+
+class CuisineSerializer(serializers.ModelSerializer):
+    """Serializer for Cuisine model to convert to/from JSON"""
+    
     class Meta:
-        model = Item
-        fields = '__all__'
+        model = Cuisine
+        fields = ['id', 'name']  # Include both id and name fields
+        # Or use: fields = '__all__' to include all fields
+        # Or exclude: exclude = ['created_at']
