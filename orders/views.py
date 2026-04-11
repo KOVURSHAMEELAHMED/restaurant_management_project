733275ff2b3612f2dc2f9a80ee372af3aba967ef
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-11 15:54:21 +0530

    69600

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework import generics, permissions
+from .models import UserReview
+from .serializers import UserReviewSerializer
 
-class MenuCategoryViewSet(viewsets.ModelViewSet):
-    queryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class MenuItemReviewCreateView(generics.CreateAPIView):
+    queryset = UserReview.objects.all()
+    serializer_class = UserReviewSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically associate the logged-in user with the review
+        serializer.save(user=self.request.user)
