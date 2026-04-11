856c570baed287a2911b473aa633364c8e256728
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-11 15:59:26 +0530

    69602

--- orders/models.py
+++ orders/models.py
@@ -1,27 +1,21 @@
 from django.db import models
-from django.core.validators import MinValueValidator
-# Import your menu model (adjust the name if it's RestaurantMenu)
-from menu.models import MenuItem 
-from .models import Order
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(
-        Order, 
-        on_delete=models.CASCADE, 
-        related_name='items'
+class DeliveryZone(models.Model):
+    name = models.CharField(
+        max_length=100, 
+        unique=True
     )
-    menu_item = models.ForeignKey(
-        MenuItem, 
-        on_delete=models.CASCADE
-    )
-    quantity = models.IntegerField(
-        default=1,
-        validators=[MinValueValidator(1)]
-    )
-    price_at_time_of_order = models.DecimalField(
+    min_order_amount = models.DecimalField(
         max_digits=10, 
         decimal_places=2
     )
+    delivery_fee = models.DecimalField(
+        max_digits=10, 
+        decimal_places=2
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
 
     def __str__(self):
-        return f'{self.quantity} x {self.menu_item.name}'
+        return self.name
