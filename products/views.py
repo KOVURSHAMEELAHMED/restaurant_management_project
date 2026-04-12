f04b3a4a9665b641c2ba787b3aaf723ffe47fa3e
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-12 10:49:36 +0530

    69784

--- products/views.py
+++ products/views.py
@@ -1,26 +1,9 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
-from .models import Item
-from .serializers import ItemSerializer
+from rest_framework import generics
+from .models import Feedback
+from .serializers import FeedbackSerializer
 
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
-
-# Create your views here.
-class ItemView(APIView):
-
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class FeedbackListView(generics.ListAPIView):
+    # Fetch all feedback, ordered by 'created_at' descending
+    queryset = Feedback.objects.all().order_by('-created_at')
+    serializer_class = FeedbackSerializer
