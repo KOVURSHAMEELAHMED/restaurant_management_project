3cd6f69b49d8b2c2b4709086a4b82f9b68050194
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-12 10:54:03 +0530

    69783

---
+++ orders/disconts.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Discount(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.code} ({self.percentage * 100}% off)"
