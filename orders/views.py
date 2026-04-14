983786ffdc6082d2ea34206309ef281024a77a25
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-14 10:23:34 +0530

    70140

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
-from .models import Ingredient
-from .serializers import IngredientSerializer
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-class IngredientViewSet(viewsets.ModelViewSet):
-    queryset = Ingredient.objects.all()
-    serializer_class = IngredientSerializer
+class FeaturedMenuItemsAPIView(generics.ListAPIView):
+    serializer_class = MenuItemSerializer
+
+    def get_queryset(self):
+        """
+        This view returns a list of all menu items
+        marked as featured.
+        """
+        return MenuItem.objects.filter(is_featured=True)
