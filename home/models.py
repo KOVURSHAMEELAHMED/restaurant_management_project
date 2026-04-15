56a77ec12dce9d048d0c66a98be6f85cf36ed583
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-15 18:20:44 +0530

    70545

--- home/models.py
+++ home/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.conf import settings # Import settings to reference user model
 
-# 1. Create the Custom Manager Class
-class MenuItemManager(models.Manager):
-    def get_budget_items(self, max_price):
-        # 2. Define the filtering logic
-        return self.filter(price__lt=max_price)
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    
-    # 3. Attach the custom manager to the model
-    objects = MenuItemManager()
-
-    def __str__(self):
-        return self.name
+class Feedback(models.Model):
+    # Existing fields ...
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,   # Allows empty values in DB
+        blank=True   # Allows empty values in forms
+    )
