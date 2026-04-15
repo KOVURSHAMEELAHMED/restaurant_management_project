9d73efe63d5dda080edfff2f3912d4a59748b5b1
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-15 18:15:54 +0530

    70546

--- home/models.py
+++ home/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    unit_of_measure = models.CharField(max_length=50)
+# 1. Create the Custom Manager Class
+class MenuItemManager(models.Manager):
+    def get_budget_items(self, max_price):
+        # 2. Define the filtering logic
+        return self.filter(price__lt=max_price)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    # 3. Attach the custom manager to the model
+    objects = MenuItemManager()
 
     def __str__(self):
         return self.name
