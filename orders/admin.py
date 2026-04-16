d01db72c068b79869cb21d05a22ffb6aadf0d5fa
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-16 11:44:08 +0530

    70624

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Ingredient, MenuItem
+from .models import Staff
 
-admin.site.register(Ingredient)
-admin.site.register(MenuItem)
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'role', 'contact_email')
+    list_filter = ('role',)
+    search_fields = ('first_name', 'last_name', 'contact_email')
