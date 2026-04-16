755ba927c94226604945aacdb0c2db92d37144cb
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-16 11:54:16 +0530

    70622

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-class Staff(models.Model):
-    ROLE_CHOICES = [
-        ('Chef', 'Chef'),
-        ('Waiter', 'Waiter'),
-        ('Manager', 'Manager'),
-    ]
-
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    contact_email = models.EmailField(unique=True)
+class DailySpecial(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    date = models.DateField(default=timezone.now().date)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.role}"
+        return self.name
