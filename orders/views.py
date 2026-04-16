85740ff4fd24337c5b2cdfdb37f8cf71e519173a
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-16 11:50:59 +0530

    70623

--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,19 @@
 from rest_framework import generics
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import OrderItem
+from .serializers import OrderItemUpdateSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-class FeaturedMenuItemsAPIView(generics.ListAPIView):
-    serializer_class = MenuItemSerializer
+class UpdateOrderItemQuantityView(generics.UpdateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemUpdateSerializer
 
-    def get_queryset(self):
-        """
-        This view returns a list of all menu items
-        marked as featured.
-        """
-        return MenuItem.objects.filter(is_featured=True)
+    def perform_update(self, serializer):
+        new_quantity = serializer.validated_data.get('quantity')
+        
+        if new_quantity == 0:
+            # Delete item if quantity is 0
+            serializer.instance.delete()
+        else:
+            # Update quantity
+            serializer.save()
