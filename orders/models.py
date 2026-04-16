96e42c7cbeef3dbf9d602d2a6e2ed52add39fec6
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-16 11:36:40 +0530

    70625

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class MenuItem(models.Model):
-    # ... existing fields ...
-    is_vegetarian = models.BooleanField(default=False)
+    name = models.CharField(max_length=200)
+    # ManyToManyField allows ingredients to be shared among menu items
+    ingredients = models.ManyToManyField(Ingredient, related_name='menu_items')
+
+    def __str__(self):
+        return self.name
