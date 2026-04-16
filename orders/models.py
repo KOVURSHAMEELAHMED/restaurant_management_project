fd0e4525b4b3b80f845931f12f2e77cf03dcca69
KOVURSHAMEELAHMED <shameelahmed17848@gmail.com>
2026-04-16 11:43:55 +0530

    70624

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+class Staff(models.Model):
+    ROLE_CHOICES = [
+        ('Chef', 'Chef'),
+        ('Waiter', 'Waiter'),
+        ('Manager', 'Manager'),
+    ]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    contact_email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.name
-
-class MenuItem(models.Model):
-    name = models.CharField(max_length=200)
-    # ManyToManyField allows ingredients to be shared among menu items
-    ingredients = models.ManyToManyField(Ingredient, related_name='menu_items')
-
-    def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name} - {self.role}"
